82c2f8212d49c34bb0a16a4ce568bdc9532f3bdc
Mahattam Tomar <mattyait@gmail.com>
2023-12-21 11:29:15 +1100

    feat: Enabling public ECR support (#4)
    * Enabling public ECR support
    
    * terraform-docs: automated action
    
    * Updating readme for public ECR usage
    
    * fixing terraform fmt for CI
    
    * fixing terraform fmt for CI
    
    * fixing the end of line error for ci
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- outputs.tf
+++ outputs.tf
@@ -1,36 +1,35 @@
-
 output "ecr_repository_arn" {
-  value       = aws_ecr_repository.this[local.image_names[0]].arn
-  description = "Full ARN of the repository."
+  description = "Full ARN of the repository"
+  value       = try(aws_ecr_repository.this[local.image_names[0]].arn, aws_ecrpublic_repository.this[local.image_names[0]].arn, null)
 }
 
 output "ecr_repository_name" {
-  value       = aws_ecr_repository.this[local.image_names[0]].name
+  value       = try(aws_ecr_repository.this[local.image_names[0]].name, aws_ecrpublic_repository.this[local.image_names[0]].name, null)
   description = "Name of first repository created"
 }
 
 output "ecr_repository_registry_id" {
-  value       = aws_ecr_repository.this[local.image_names[0]].registry_id
+  value       = try(aws_ecr_repository.this[local.image_names[0]].registry_id, aws_ecrpublic_repository.this[local.image_names[0]].registry_id, null)
   description = "The registry ID where the repository was created."
 }
 
 output "ecr_repository_url" {
-  value       = aws_ecr_repository.this[local.image_names[0]].repository_url
+  value       = try(aws_ecr_repository.this[local.image_names[0]].repository_url, aws_ecrpublic_repository.this[local.image_names[0]].repository_url, null)
   description = "URL of first repository created"
 }
 
 output "repository_url_map" {
   value = zipmap(
-    values(aws_ecr_repository.this)[*].name,
-    values(aws_ecr_repository.this)[*].repository_url
+    concat(values(aws_ecr_repository.this)[*].name, values(aws_ecrpublic_repository.this)[*].repository_name),
+    concat(values(aws_ecr_repository.this)[*].repository_url, values(aws_ecrpublic_repository.this)[*].repository_uri)
   )
   description = "Map of repository names to repository URLs"
 }
 
 output "repository_arn_map" {
   value = zipmap(
-    values(aws_ecr_repository.this)[*].name,
-    values(aws_ecr_repository.this)[*].arn
+    concat(values(aws_ecr_repository.this)[*].name, values(aws_ecrpublic_repository.this)[*].repository_name),
+    concat(values(aws_ecr_repository.this)[*].arn, values(aws_ecrpublic_repository.this)[*].arn)
   )
   description = "Map of repository names to repository ARNs"
 }
